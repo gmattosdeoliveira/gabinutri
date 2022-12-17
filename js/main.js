d32df9c30f497f21fb13d17ab5441a3b112138c9
gmattosdeoliveira <65202237+gmattosdeoliveira@users.noreply.github.com>
2022-12-16 22:02:27 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+//Swiper slider
+var swiper = new Swiper(".bg-slider-thumbs", {
+    loop: true,
+    spaceBetween: 0,
+    slidesPerView: 0,
+});
+var swiper2 = new Swiper(".bg-slider", {
+    loop: true,
+    spaceBetween: 0,
+    autoplay: {
+        delay: 7000,
+        disableOnInteraction: false,
+      },
+    thumbs: {
+        swiper: swiper,
+    },
+});
+
+//Navigation bar effects on scroll
+
+//Responsive navigation menu toggle
